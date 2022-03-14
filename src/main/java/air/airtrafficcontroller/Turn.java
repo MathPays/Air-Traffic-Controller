cfa1397df14f56e0ec85c8c3d9ee3efc7ee3a8b8
Mathilde Paysant <57558201+MathPays@users.noreply.github.com>
2022-03-13 22:35:44 -0400

    Delete src directory

--- src/main/java/air/airtrafficcontroller/Turn.java
+++
@@ -1,5 +0,0 @@
-package air.airtrafficcontroller;
-
-public class Turn {
-
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module air.airtrafficcontroller {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens air.airtrafficcontroller to javafx.fxml;
-    exports air.airtrafficcontroller;
-}
